3194d5374a0ab10d05ee40a06088b2f1b9070dc0
Aron <gorogaron95@gmail.com>
2018-02-14 15:45:58 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Neural Network project
+@author: aron, joci
+"""
+
+import numpy as np
+import tensorflow as tf
+import keras
+
+if __name__ == '__main__':
+
+	
