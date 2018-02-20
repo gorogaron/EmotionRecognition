4666396737b79fbfb3a05cf3d9ed7ec57af600a5
kenderak <joseph.kenderak@gmail.com>
2018-02-20 17:47:50 +0100

    Pre-process step 1

---
+++ createFolders.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 22:23:19 2018
+
+@author: kenderak
+"""
+import sys
+import os
+import shutil
+
+"print(os.path.abspath('createFolders.py'))"
+
+
+
+if not os.path.exists('dataToProcess'):
+    os.mkdir('dataToProcess')
+
+emotions = {  'W' : 'anger', 
+              'L' : 'boredom', 
+              'E' : 'disgust',
+              'A' : 'fear',
+              'F' : 'happiness',
+              'T' : 'sadness',
+              'N' : 'neutral'  }
+
+for key, value in emotions.items():
+    name = os.path.abspath('.')+ '/dataToProcess/' + value
+    if not os.path.exists(name):
+        os.mkdir(name)
+        print("Created dir : " + value)
+
+print("Directories have been created.")
+print("Copying files...")
+
+
+for root, dirs, files in os.walk(os.path.abspath('.') + '/dataset/wav'):
+    for file in files:
+        key = file[5]
+        srcFile = root + '/' + file
+        dstFile = os.path.abspath('.') + '/dataToProcess' + '/' + emotions[key] + '/' + file        
+        shutil.copy(srcFile, dstFile)
+        print("Copying " + file)
--- main.py
+++ main.py
@@ -8,7 +8,33 @@ Neural Network project
 import numpy as np
 import tensorflow as tf
 import keras
+import matplotlib.pyplot as plot
+from scipy.signal import stft
+from scipy.io.wavfile import read as readWav
 
-if __name__ == '__main__':
+wavPath = '/usr/share/sounds/ekiga/voicemail.wav'
+fileName = '/home/kenderak/_repos/EmotionRecognition/dataset/wav/03a01Fa.wav'
+freq, data = readWav(fileName)
 
-	
+plot.plot(data)
+plot.title('Time domain')
+plot.ylabel('Amplitude')
+plot.xlabel('Time [sec]')
+plot.grid()
+plot.axis('tight')
+plot.show()
+
+fs = 16000
+
+
+f, t, Zxx = stft(data, fs, nperseg=128, window = 'cosine', noverlap = 0.5 * 128)
+plot.pcolormesh(t, f, np.angle(Zxx), vmin=0, vmax=np.max(np.angle(Zxx)))
+plot.title('Spectrogram')
+plot.ylabel('Frequency [Hz]')
+plot.xlabel('Time [sec]')
+plot.axis('tight')
+plot.show()
+t = np.transpose(np.arange(0.00, 1.9, 1/250))
+plot.plot(t,abs(Zxx[1,1:476]))
+
+
